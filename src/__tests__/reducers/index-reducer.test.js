4720c32dc18678477bb6757b80f9410626d4acea
David Sterry <sterry.david@gmail.com>
2021-02-16 13:44:09 -0800

    pass getting reducers default state
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import postListReducer from '../../reducers/post-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe ('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -6,5 +11,11 @@ describe ('rootReducer', () => {
       masterPostList: {},
       formVisibleOnPage: false
     });
+  });
+  test('Check that initial state of postListReducers matches root reducer', ()=> {
+    expect(store.getState().masterPostList).toEqual(postListReducer(undefined, { type: null }));
+  });
+  test('Check that initial state of formVisibleReducer matches root reducer', ()=>{
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,9 @@ import formVisibleReducer from './form-visible-reducer';
 import postListReducer from './post-list-reducer';
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterPostList: postListReducer
+});
 
 export default rootReducer;
