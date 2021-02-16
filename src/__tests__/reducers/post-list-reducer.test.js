8be9c7e64d2770546d88098777683fd8e31b1076
David Sterry <sterry.david@gmail.com>
2021-02-16 11:30:14 -0800

    good fail for Add Post method
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -2,7 +2,38 @@ import postListReducer from '../../reducers/post-list-reducer';
 
 
 describe('postListReducer', () => {
+
+  let action;
+
+  const postData = {
+    title: "All About Rye",
+    body: "It's the best bread",
+    votecount: 5,
+    timestamp: "2/16/2021 2:28",
+    id: 1
+  }
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(postListReducer({}, {type: null})).toEqual({});
-    });
   });
+  
+  test('Should add a post to our masterPostList', ()=> {
+    const { title, body, votecount, timestamp, id } = postData;
+    action ={
+      type: 'ADD_POST',
+      title,
+      body,
+      votecount,
+      timestamp,
+      id
+    }
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        title,
+        body,
+        votecount,
+        timestamp,
+        id
+      }
+    })
+  });  
+});
