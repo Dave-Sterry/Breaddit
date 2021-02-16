0225415781abe90573bcab7954bc2d3c26848538
David Sterry <sterry.david@gmail.com>
2021-02-16 11:33:04 -0800

    pass Add Post test
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const {title, body, votecount, timestamp, id } = action;
+  switch(action.type){
+    case 'ADD_POST':
+      return Object.assign({}, state, {
+        [id]: {
+          title,
+          body,
+          votecount,
+          timestamp,
+          id
+        }
+      });
+      default:
+        return state;
+    }
+  
 };
