c5130ba1a1dd719daa05e29f8ea0e9fd94e68fe4
David Sterry <sterry.david@gmail.com>
2021-02-17 09:37:05 -0800

    fail/pass Toggle Action creator
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -2,9 +2,14 @@ import * as a from './../../actions';
 
 describe('breaddit actions', () => {
   it('DELETE_POST should create DELETE_POST action', () => {
-      expect(a.deletePost(1)).toEqual({
-        type: 'DELETE_POST',
-        id: 1
-      });
+    expect(a.deletePost(1)).toEqual({
+      type: 'DELETE_POST',
+      id: 1
+    });
+  });
+  it('toggleForm should create TOGGLE_FORM action', ()=>{
+    expect(a.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
   });
 });
